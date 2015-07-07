d844a1d8f561dcc61401e58613b0c3c13d83b272
zigzagpig <695312886@qq.com>
2015-07-07 23:20:19 +0800

    完成了第三章的练习

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About #{@main_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact #{@main_title}"
+  end
+
+
 end
