5fb0b528b76fa545bbce0154c8319daf404b97d0
zigzagpig <695312886@qq.com>
2015-07-07 22:58:27 +0800

    完成了静态页面

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home 小斗鱼"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help 小斗鱼"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+    assert_select "title", "About 小斗鱼"
   end
 
 end
