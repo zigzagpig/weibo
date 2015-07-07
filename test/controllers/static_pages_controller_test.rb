5851cbbd035b8f4efd738de132c38a21c85334e8
zigzagpig <695312886@qq.com>
2015-07-07 23:14:30 +0800

    使静态页面控制器中的测试文件去除重复的文字

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @main_title = "小斗鱼"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home 小斗鱼"
+    assert_select "title", "Home #{@main_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help 小斗鱼"
+    assert_select "title", "Help #{@main_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About 小斗鱼"
+    assert_select "title", "About #{@main_title}"
   end
 
 end
